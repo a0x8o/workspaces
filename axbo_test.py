4afe769601c8d81f706aafe3a4b8b815b2602196
a0x8o <a0x8o>
2021-02-18 22:49:45 +0000

    Initial commit axbo_test

---
+++ axbo_test.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+import datetime
+print ("This was last run on: %s" % datetime.datetime.now())
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("/databricks-datasets/samples/docs/"))
+
+# COMMAND ----------
+
+textFile = sc.textFile("/databricks-datasets/samples/docs/README.md")
+
+# COMMAND ----------
+
+textFile.count()
+
+# COMMAND ----------
+
+textFile.first()
+
+# COMMAND ----------
+
+linesWithSpark = textFile.filter(lambda line: "Spark" in line)
